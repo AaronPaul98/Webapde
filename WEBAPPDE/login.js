3b2d86f436ccf5b45c7951469ed91b0215c685f8
AaronPaul98 <41133485+AaronPaul98@users.noreply.github.com>
2018-07-13 23:27:37 +0800

    Add files via upload

---
+++ WEBAPPDE/login.js
@@ -0,0 +1,10 @@
+	
+
+	checkLogin function(uName,password){
+
+		if(uName == "Aaron" && password == "123"){ return 1;}
+		if(uName == "Adrian" && password == "123"){ return 1;}
+		if(uName == "Adriel" && password == "123"){ return 1;}
+		if(uName == "Andrea" && password == "123"){ return 1;}
+		if(uName == "Angelo" && password == "123"){ return 1;}
+	}
